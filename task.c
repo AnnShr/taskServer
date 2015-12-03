8af917f7fd588919210438d09f75631748a597cf
AnnShr <annshr@yandex.ru>
2015-12-03 16:52:48 +0300

    update task.c

--- task.c
+++ task.c
@@ -1,3 +1,90 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#include<signal.h>
+#include<fcntl.h>
+
+#define CONNMAX 5
+#define BYTES 1024
+
+char *ROOT;
+int listenfd, clients[CONNMAX];
+pthread_t ntid[5];
+pthread_t servtid;
+char *defaultport="6565";
+
+void error(char *);
+void startServer(char *);
+void *respond(void *);
+void createThread(int);
+
+
+int main(int argc, char* argv[])
+{
+    struct sockaddr_in clientaddr;
+    socklen_t addrlen;
+	
+    char c;  
+    int j=0;	
+    
+    //Default Values PATH = ~/ and PORT=10000
+    char PORT[6];
+    ROOT = getenv("PWD");
+    strcpy(PORT,defaultport);
+
+    int slot=0;
+
+    //Parsing the command line arguments
+    while ((c = getopt (argc, argv, "p:r:")) != -1)
+        switch (c)
+        {
+            case 'r':
+                ROOT = malloc(strlen(optarg));
+                strcpy(ROOT,optarg);
+                break;
+            case 'p':
+                strcpy(PORT,optarg);
+                break;
+            case '?':
+                fprintf(stderr,"Wrong arguments given!!!\n");
+                exit(1);
+            default:
+                exit(1);
+        }
+    
+    printf("Server started at port no. %s%s%s with root directory as %s%s%s\n","\033[92m",PORT,"\033[0m","\033[92m",ROOT,"\033[0m");
+    // Setting all elements to -1: signifies there is no client connected
+    int i;
+    for (i=0; i<CONNMAX; i++)
+        clients[i]=-1;
+    startServer(PORT);
+
+    // ACCEPT connections
+    while (1)
+    {
+        addrlen = sizeof(clientaddr);
+        clients[slot] = accept (listenfd, (struct sockaddr *) &clientaddr, &addrlen);
+
+        if (clients[slot]<0)
+            error ("accept() error");
+        else
+        {
+		createThread(slot);
+		}
+        while (clients[slot]!=-1) slot = (slot+1)%CONNMAX;
+	}
+    return 0;
+}
+
+
+//create
+
 //start server
 void startServer(char *port)
 {
@@ -33,4 +120,57 @@ void startServer(char *port)
         perror("listen() error");
         exit(1);
     }
+}
+
+//client connection
+void *respond(void* arg)
+{
+    char mesg[99999], *reqline[3], data_to_send[BYTES], path[99999];
+    int rcvd, fd, bytes_read;
+    int *p = (int *) arg;
+	int n = *p;
+    memset( (void*)mesg, (int)'\0', 99999 );
+
+    rcvd=recv(clients[n], mesg, 99999, 0);
+
+    if (rcvd<0)    // receive error
+        fprintf(stderr,("recv() error\n"));
+    else if (rcvd==0)    // receive socket closed
+        fprintf(stderr,"Client disconnected upexpectedly.\n");
+    else    // message received
+    {
+        printf("%s", mesg);
+        reqline[0] = strtok (mesg, " \t\n");
+        if ( strncmp(reqline[0], "GET\0", 4)==0 )
+        {
+            reqline[1] = strtok (NULL, " \t");
+            reqline[2] = strtok (NULL, " \t\n");
+            if ( strncmp( reqline[2], "HTTP/1.0", 8)!=0 && strncmp( reqline[2], "HTTP/1.1", 8)!=0 )
+            {
+                write(clients[n], "HTTP/1.0 400 Bad Request\n", 25);
+            }
+            else
+            {
+                if ( strncmp(reqline[1], "/\0", 2)==0 )
+                    reqline[1] = "/index.html";        //Because if no file is specified, index.html will be opened by default (like it happens in APACHE...
+
+                strcpy(path, ROOT);
+                strcpy(&path[strlen(ROOT)], reqline[1]);
+                printf("file: %s\n", path);
+
+                if ( (fd=open(path, O_RDONLY))!=-1 )    //FILE FOUND
+                {
+                    send(clients[n], "HTTP/1.0 200 OK\n\n", 17, 0);
+                    while ( (bytes_read=read(fd, data_to_send, BYTES))>0 )
+                        write (clients[n], data_to_send, bytes_read);
+                }
+                else    write(clients[n], "HTTP/1.0 404 Not Found\n", 23); //FILE NOT FOUND
+            }
+        }
+    }
+
+    //Closing SOCKET
+    shutdown (clients[n], SHUT_RDWR);         //All further send and recieve operations are DISABLED...
+    close(clients[n]);
+    clients[n]=-1;
 }
